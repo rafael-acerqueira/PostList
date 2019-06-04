c9e3babaa7ba5a32a686c42bb7289b2fc38b9b1d
Rafael <rafael.acerqueirah@gmail.com>
2019-06-04 10:36:33 -0300

    Change Class Component to Hook Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 import Wrapper from './Wrapper'
 import Post from './Post'
 
-class App extends Component {
-	render () {
-		return (
-			<>
-			<Header>Post List</Header>
-			<Wrapper>
-				<Post />
-			</Wrapper>
-			</>
-		)
-	}
+const App = () => {
+	const [posts, setPosts] = useState([])
+
+	return (
+		<>
+		<Header>Post List</Header>
+		<Wrapper>
+			<Post />
+		</Wrapper>
+	</>
+	)
 }
 
 export default App
