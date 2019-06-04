715a38dd736eae6e815a5ae20a169abddcf6e330
Rafael <rafael.acerqueirah@gmail.com>
2019-06-04 10:34:39 -0300

    Create Wrapper and Post component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
 import Header from './Header'
+import Wrapper from './Wrapper'
+import Post from './Post'
 
 class App extends Component {
-  render () {
-    return <Header>Post List</Header>
-  }
+	render () {
+		return (
+			<>
+			<Header>Post List</Header>
+			<Wrapper>
+				<Post />
+			</Wrapper>
+			</>
+		)
+	}
 }
 
 export default App
---
+++ src/components/Wrapper.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+	background-color: #F2F3F5;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 100vh;
+`
+
+export default Wrapper
